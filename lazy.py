c07fc3eacb37dfce396160877a40029e3306651d
memoryleak47 <memoryleak47@protonmail.ch>
2016-04-19 00:12:05 +0200

    lazy.py: ordered stuff

--- lazy.py
+++ lazy.py
@@ -3,22 +3,19 @@
 import sys
 import math
 
+# changeable
+
 MIN_COMPLEXITY = 0
 MAX_COMPLEXITY = 6
-ARGS_AMOUNT = 1
 TOLERANCE = 0.03
-
 FUNCS = ["($+$)", "($-$)"]
 VARS=["1"]
-
-for arg in range(ARGS_AMOUNT):
-	VARS.append("args[" + str(arg) + "]")
 
 RULES = list()
 RULES.append(([3], 2))
 RULES.append(([5], 4))
 
-OVERFLOW=True
+# main code
 
 def die(string):
 	print(string)
@@ -48,25 +45,25 @@ class FunctionCreator():
 		if len(self.args) > 2:
 			die("too many args")
 
-	def __increase(self):
+	def __increase(self): # returns whether it is now overflowed
 		if self.complexity == 0:
-			return OVERFLOW
+			return True
 		# increase args
 		for arg in self.args:
-			if arg.__increase() != OVERFLOW:
-				return not OVERFLOW
+			if arg.__increase() != True:
+				return False
 		# rearrangement
 		if len(self.args) == 2 and self.args[1].complexity != self.complexity-1:
 			self.args[0] = FunctionCreator(self.args[0].complexity-1)
 			self.args[1] = FunctionCreator(self.args[1].complexity+1)
-			return not OVERFLOW
+			return False
 		# reset
 		if self.counter+1 > len(FUNCS)-1:
 			self.__updateCounter(0)
-			return OVERFLOW
+			return True
 		# update
 		self.__updateCounter(self.counter+1)
-		return not OVERFLOW
+		return False
 
 	def __get(self):
 		if self.complexity == 0:
@@ -84,7 +81,7 @@ class FunctionCreator():
 		return tmp
 
 	def getNext(self):
-		return self.__get(), (self.__increase() == OVERFLOW)
+		return self.__get(), self.__increase()
 
 def validate(func):
 	for rule in RULES:
@@ -96,34 +93,41 @@ def validate(func):
 			return False
 	return True
 
-for complexity in range(MIN_COMPLEXITY, MAX_COMPLEXITY+1):
-	print("complexity=" + str(complexity))
-	fc = FunctionCreator(complexity)
-	while True:
-		func, overflow = fc.getNext()
 
-		# insertVars
-		varIndexes=list()
-		for x in range(func.count("$")):
-			varIndexes.append(0)
+if __name__ == "__main__":
+	ARGS_AMOUNT = len(RULES[0][0])
+
+	for arg in range(ARGS_AMOUNT):
+		VARS.append("args[" + str(arg) + "]")
+
+	for complexity in range(MIN_COMPLEXITY, MAX_COMPLEXITY+1):
+		print("Complexity = " + str(complexity))
+		fc = FunctionCreator(complexity)
 		while True:
-			varfunc = func
-			for i in varIndexes:
-				varfunc = varfunc[:varfunc.find("$")] + VARS[i] + varfunc[varfunc.find("$")+1:]
-			if validate(varfunc):
-				print(varfunc)
-				sys.exit()
-			else:
-				checkedAllVars=True
-				for i in range(len(varIndexes)):
-					if varIndexes[i] == len(VARS)-1:
-						varIndexes[i] = 0
-					else:
-						varIndexes[i] += 1
-						checkedAllVars=False
+			func, overflow = fc.getNext()
+
+			# insertVars
+			varIndexes=list()
+			for x in range(func.count("$")):
+				varIndexes.append(0)
+			while True:
+				varfunc = func
+				for i in varIndexes:
+					varfunc = varfunc[:varfunc.find("$")] + VARS[i] + varfunc[varfunc.find("$")+1:]
+				if validate(varfunc):
+					print(" " + varfunc)
+					sys.exit()
+				else:
+					checkedAllVars=True
+					for i in range(len(varIndexes)):
+						if varIndexes[i] == len(VARS)-1:
+							varIndexes[i] = 0
+						else:
+							varIndexes[i] += 1
+							checkedAllVars=False
+							break
+					if checkedAllVars:
 						break
-				if checkedAllVars:
-					break
-		if overflow == OVERFLOW:
-			break
-print("Game over")
+			if overflow:
+				break
+	print("Game over")
